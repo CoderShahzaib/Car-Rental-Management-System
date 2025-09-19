c80aa91eda6a2fd24e7de2b8be615fb6c1cef94d
CoderShahzaib <your-github-email@example.com>
2025-09-19 12:01:42 +0500

    fixe update customers

--- api/updateCustomers.js
+++ api/updateCustomers.js
@@ -1,15 +1,18 @@
+
 export default async function handler(req, res) {
   if (req.method !== "PUT") {
     return res.status(405).json({ message: "Method not allowed" });
   }
-
   try {
-    const body = await req.json();
-    const response = await fetch("https://freeapi.miniprojectideas.com/api/CarRentalApp/UpdateCustomer", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    });
+    const body = req.body; 
+    const response = await fetch(
+      `https://freeapi.miniprojectideas.com/api/CarRentalApp/UpdateCustomer`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      }
+    );
     const data = await response.json();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
